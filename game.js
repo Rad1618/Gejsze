23ff6d2310ab976058ab23112775eade5248537f
Rad1618 <81822443+Rad1618@users.noreply.github.com>
2022-02-08 12:45:46 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ function Start_game_host()
 {
     public_data.game_started = true;
     Send_message('update', public_data);
-    private_data.my_turn = Math.round(Math.random());   //losowanie gracza zaczynaj¹cego
+    private_data.my_turn = Math.round(Math.random());   //losowanie gracza zaczynajÄ…cego
     return Start_round_host();
 }
 
@@ -35,7 +35,7 @@ function Start_round_host()
         [deck[i], deck[random_index]] = [deck[random_index], deck[i]];
     }
     other_deck = [];
-    for (var i = 1; i < deck.length; i++)   //karta 0 nie bierze udzia³u w grze
+    for (var i = 1; i < deck.length; i++)   //karta 0 nie bierze udziaÅ‚u w grze
     {
         if (i % 2 === 0)    //rozdawanie kart
             private_data.deck.push(deck[i]);
@@ -59,7 +59,7 @@ function Start_round()
     private_data.enemy_safe_card = -1;
     private_data.safe_cards_visible = false;
     private_data.hand += 6;
-    Next_turn();    //rozpoczêcie nastêpnej tury
+    Next_turn();    //rozpoczÄ™cie nastÄ™pnej tury
     Update_windows();
 }
 
@@ -86,7 +86,7 @@ function Next_turn()
         }
         else   //koniec kart
         {
-            Send_message('end_round', null); //wysy³anie informacji o rozpoczêciu podsumowania
+            Send_message('end_round', null); //wysyÅ‚anie informacji o rozpoczÄ™ciu podsumowania
         }
     }
     Update_windows();
@@ -94,7 +94,7 @@ function Next_turn()
 
 function End_round()
 {
-    private_data.gifts[private_data.safe_card]++;   //dodanie bezpiecznych kart do prezentów
+    private_data.gifts[private_data.safe_card]++;   //dodanie bezpiecznych kart do prezentÃ³w
     private_data.enemy_gifts[private_data.enemy_safe_card]++;
     private_data.safe_cards_visible = true;
     for (var i = 0; i < cards.numbers.length; i++)
@@ -127,7 +127,7 @@ function End_round_host()
         }
     }
     //console.log(my_number + " " + my_points + " " + enemy_number + " " + enemy_points);
-    if (my_points >= 11)    //sprawdzanie, czy któryœ gracz wygra³
+    if (my_points >= 11)    //sprawdzanie, czy ktÃ³ryÅ› gracz wygraÅ‚
         Send_message('winner', private_data.host);
     else if (enemy_points >= 11)
         Send_message('winner', !private_data.host);
@@ -149,7 +149,7 @@ function Board_click(pos_x, pos_y)
         switch (private_data.mode)
         {
             case 'action':
-                for (var i = 0; i < 4; i++) //klikniêcie na akcjê
+                for (var i = 0; i < 4; i++) //klikniÄ™cie na akcjÄ™
                 {
                     if (private_data.actions[i] === 'active' && pos_x >= (i + 1) * 50 && pos_x <= (i + 1) * 50 + 40 && pos_y >= 500 && pos_y <= 540)
                     {
@@ -174,14 +174,14 @@ function Board_click(pos_x, pos_y)
                                 private_data.safe_card = private_data.deck[i];
                                 break;
                             case 2: //likwidacja kart
-                                //wszystko to usówanie karty, co zosta³o wykonane pod spodem
+                                //wszystko to usÃ³wanie karty, co zostaÅ‚o wykonane pod spodem
                                 break;
                             case 3: case 4:
                                 private_data.cards_to_send.push(private_data.deck[i]);
                                 break;
                         }
                         private_data.hand--;
-                        private_data.deck.splice(i, 1); //usówanie karty z rêki
+                        private_data.deck.splice(i, 1); //usÃ³wanie karty z rÄ™ki
                         private_data.card_counter--;
                         if (private_data.card_counter <= 0)
                         {
@@ -193,8 +193,8 @@ function Board_click(pos_x, pos_y)
                                 private_data.cards_to_send = [];
                             }
                             else if (private_data.mode_type === 1)
-                                Send_message('safe_card', private_data.safe_card, private_data.host);   //wys³anie informacji o bezpiecznej karcie
-                            else if (private_data.mode_type === 2)  //tylko akcja 2. wysy³a tu informacjê o koñcu rundy
+                                Send_message('safe_card', private_data.safe_card, private_data.host);   //wysÅ‚anie informacji o bezpiecznej karcie
+                            else if (private_data.mode_type === 2)  //tylko akcja 2. wysyÅ‚a tu informacjÄ™ o koÅ„cu rundy
                                 Send_message('next_turn', null);
                         }
                         Update_windows();
@@ -203,30 +203,38 @@ function Board_click(pos_x, pos_y)
                 break;
         }
     }
-    else if (private_data.cards_to_send.length > 0) //drugi gracz ma wybór do wykonania
+    else if (private_data.cards_to_send.length > 0) //drugi gracz ma wybÃ³r do wykonania
     {
         for (var i = 0; i < private_data.cards_to_send.length; i++)
         {
-            if (pos_x >= 275 + 30 * i && pos_x <= 300 + 30 * i && pos_y >= 25 && pos_y <= 50)
+            if (private_data.cards_to_send.length === 3)
             {
-                if (private_data.cards_to_send.length === 3)
+                if (pos_x >= 275 + 30 * i && pos_x <= 300 + 30 * i && pos_y >= 25 && pos_y <= 50)
                 {
                     private_data.gifts[private_data.cards_to_send[i]]++;    //dodanie prezentu dla gejszy
                     Update_gifts();
                     private_data.cards_to_send.splice(i, 1);
+                    Send_message('send_cards', private_data.cards_to_send);
+                    private_data.cards_to_send = [];
+                    Update_windows();
+                    break;
                 }
-                else if (private_data.cards_to_send.length === 4)
+            }
+            else if (private_data.cards_to_send.length === 4)
+            {
+                if (pos_x >= 275 + 30 * i + Math.floor(i / 2) * 20 && pos_x <= 300 + 30 * i + Math.floor(i / 2) * 20 && pos_y >= 25 && pos_y <= 50)
                 {
-                    i = Math.floor(i / 2);
-                    private_data.gifts[private_data.cards_to_send[i]]++;    //dodanie prezentów dla gejszy
+                    i = Math.floor(i / 2) * 2;
+                    private_data.gifts[private_data.cards_to_send[i]]++;    //dodanie prezentÃ³w dla gejszy
                     private_data.gifts[private_data.cards_to_send[i + 1]]++;
                     Update_gifts();
                     private_data.cards_to_send.splice(i, 2);
+                    Send_message('send_cards', private_data.cards_to_send);
+                    private_data.cards_to_send = [];
+                    Update_windows();
+                    break;
                 }
-                Send_message('send_cards', private_data.cards_to_send);
-                private_data.cards_to_send = [];
-                Update_windows();
-            }
+            }  
         }
     }
 }
